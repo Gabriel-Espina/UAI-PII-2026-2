a0259ba6a4987898c4b333594a834284b53787d8
barrios115 <42774124+barrios115@users.noreply.github.com>
2026-08-29 19:13:47 -0300

    Add files via upload
    Agrego ejercicios 10, 11 y 12

---
+++ tps/tp1/grupo4/Program.cs
@@ -0,0 +1,13 @@
+﻿class Ejercicio_12
+{
+    static void Main()
+    {
+        DateTime fechaHora;
+
+        fechaHora = DateTime.Now;
+
+        Console.WriteLine("Fecha y hora actual: " + fechaHora);
+
+        Console.ReadKey();
+    }
+}
