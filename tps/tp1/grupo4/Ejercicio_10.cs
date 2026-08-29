f1830c15d72e3643636e4d5c713af55f2052e881
barrios115 <42774124+barrios115@users.noreply.github.com>
2026-08-29 20:16:55 -0300

    Add files via upload
    Agrego ejercicios 10, 11

---
+++ tps/tp1/grupo4/Ejercicio_10.cs
@@ -0,0 +1,22 @@
+﻿class Ejercicio_1
+{
+    static void Main()
+    {
+        string x;
+        int mitad;
+        string segundaMitad;
+        string primeraMitad;
+
+        Console.Write("Ingrese una frase: ");
+        x = Console.ReadLine();
+
+        mitad = x.Length / 2;
+
+        primeraMitad = x.Substring(0, mitad);
+        segundaMitad = x.Substring(mitad);
+
+        Console.WriteLine("Resultado: " + segundaMitad + primeraMitad);
+
+        Console.ReadKey();
+    }
+}
---
+++ tps/tp1/grupo4/Ejercicio_11.cs
@@ -0,0 +1,18 @@
+﻿class Ejercicio_11
+{
+    static void Main()
+    {
+        string frase;
+        int cantidad;
+
+        Console.Write("Ingrese una frase: ");
+        frase = Console.ReadLine();
+
+        cantidad = frase.Length;
+
+        Console.WriteLine("La cantidad de caracteres es: " + cantidad);
+
+        Console.ReadKey();
+    }
+}
+
