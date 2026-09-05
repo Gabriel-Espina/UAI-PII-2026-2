0fd895d1c2fde6b00194f3fc94c5a93d66a9c415
RocioQuinteros <roqui257@gmail.com>
2026-09-05 14:05:17 -0300

    Agrego ejercicio 1

---
+++ tps/tp3/grupo4/ejercicio1/TPN°3_ejercicio1/TPN°3_ejercicio1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TPN_3_ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSuma.Location = new System.Drawing.Point(116, 116);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(184, 91);
+            this.btnSuma.TabIndex = 2;
+            this.btnSuma.Text = "Sumar";
+            this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(112, 62);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(208, 20);
+            this.lbl2.TabIndex = 4;
+            this.lbl2.Text = "SUMA DE DOS NÚMEROS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(427, 260);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.btnSuma);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Label lbl2;
+    }
+}
+
---
+++ tps/tp3/grupo4/ejercicio1/TPN°3_ejercicio1/TPN°3_ejercicio1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPN_3_ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            string texto1;
+            string texto2;
+            double numero1;
+            double numero2;
+
+            do
+            {
+                texto1 = Microsoft.VisualBasic.Interaction.InputBox("Ingrese el primer valor: ");
+                if (texto1 == "")
+                {
+                    MessageBox.Show("Debe ingresar un valor");
+                    return;
+                }
+                else if (!double.TryParse(texto1, out numero1))
+                {
+                    MessageBox.Show("Debe ingresar un valor numérico");
+                    return;
+                }
+            }
+            while (texto1 == "" || !double.TryParse(texto1, out numero1));
+            
+
+            do
+            {
+                texto2 = Microsoft.VisualBasic.Interaction.InputBox("Ingrese el segundo valor: ");
+                if (texto2 == "")
+                {
+                    MessageBox.Show("Debe ingresar un valor");
+                    return;
+                }
+                else if (!double.TryParse(texto2, out numero2))
+                {
+                    MessageBox.Show("Debe ingresar un valor numérico");
+                    return;
+                }
+            } while(texto2 == "" || !double.TryParse(texto2, out numero2));
+            
+            double sumar = numero1  + numero2;
+            MessageBox.Show("La suma es: " + sumar);
+        }
+    }
+}
