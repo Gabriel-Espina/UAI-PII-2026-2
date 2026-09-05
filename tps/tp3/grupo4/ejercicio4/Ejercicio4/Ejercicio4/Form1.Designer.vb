3e41ad4d2b51a4717dee508297304001f2fb379e
Matias Nicolas Acuna <matiasnicolasacuna@MacBook-de-Matias.local>
2026-09-05 17:36:13 -0300

    Subo archivos tp 2 y 3

---
+++ tps/tp3/grupo4/ejercicio4/Ejercicio4/Ejercicio4/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(33, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(140, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Ingrese el primer numero"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(33, 48)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(173, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(33, 165)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(173, 23)
+        TextBox2.TabIndex = 2
+        TextBox2.Text = "  "
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(33, 126)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(151, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Ingrese el segundo numero"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(38, 217)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Suma"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(131, 217)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 5
+        Button2.Text = "Resta"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(228, 217)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(98, 23)
+        Button3.TabIndex = 6
+        Button3.Text = "Multiplicacion"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(347, 217)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 7
+        Button4.Text = "Division"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(440, 217)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 8
+        Button5.Text = "Potencia"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(530, 217)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(108, 23)
+        Button6.TabIndex = 9
+        Button6.Text = "Raiz Cuadrada"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        ForeColor = SystemColors.ControlText
+        Name = "Form1"
+        Text = "Ejercicio4"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+
+End Class
---
+++ tps/tp3/grupo4/ejercicio4/Ejercicio4/Ejercicio4/Form1.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2 As Double
+        If Double.TryParse(TextBox1.Text, num1) AndAlso Double.TryParse(TextBox2.Text, num2) Then
+            MessageBox.Show("Resultado: " & (num1 + num2).ToString())
+        Else
+            MessageBox.Show("Ingresa números válidos")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim num1, num2 As Double
+        If Double.TryParse(TextBox1.Text, num1) AndAlso Double.TryParse(TextBox2.Text, num2) Then
+            MessageBox.Show("Resultado: " & (num1 - num2).ToString())
+        Else
+            MessageBox.Show("Ingresa números válidos")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim num1, num2 As Double
+        If Double.TryParse(TextBox1.Text, num1) AndAlso Double.TryParse(TextBox2.Text, num2) Then
+            MessageBox.Show("Resultado: " & (num1 * num2).ToString())
+        Else
+            MessageBox.Show("Ingresa números válidos")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim num1, num2 As Double
+        If Double.TryParse(TextBox1.Text, num1) AndAlso Double.TryParse(TextBox2.Text, num2) Then
+            If num2 <> 0 Then
+                MessageBox.Show("Resultado: " & (num1 / num2).ToString())
+            Else
+                MessageBox.Show("No se puede dividir por cero")
+            End If
+        Else
+            MessageBox.Show("Ingresa números válidos")
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim baseNum, exponente As Double
+        If Double.TryParse(TextBox1.Text, baseNum) AndAlso Double.TryParse(TextBox2.Text, exponente) Then
+            Dim resultado As Double = Math.Pow(baseNum, exponente)
+            MessageBox.Show("Resultado: " & resultado.ToString())
+        Else
+            MessageBox.Show("Ingresa números válidos")
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim num1 As Double
+        If Double.TryParse(TextBox1.Text, num1) Then
+            If num1 >= 0 Then
+                Dim resultado As Double = Math.Sqrt(num1)
+                MessageBox.Show("Raíz del 1er número: " & resultado.ToString())
+            Else
+                MessageBox.Show("No existe raíz de un número negativo")
+            End If
+        Else
+            MessageBox.Show("Ingresa un número válido en el primer cuadro")
+        End If
+    End Sub
+End Class
