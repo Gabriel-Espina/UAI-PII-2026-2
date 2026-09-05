100dae2008aac9ad3677fab211aa2651dc02bc33
RocioQuinteros <roqui257@gmail.com>
2026-09-04 21:17:59 -0300

    Agrego ejercicio 11_TATETI

---
+++ tps/tp3/grupo4/ejercicio11/T.PN°3_ejercicio_11/T.PN°3_ejercicio_11/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace T.PN_3_ejercicio_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.lblEmpate = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 130F));
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(61, 38);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 96F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(390, 300);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn9.Location = new System.Drawing.Point(263, 207);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(124, 90);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(124, 96);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn8.Location = new System.Drawing.Point(133, 207);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(124, 90);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn7.Location = new System.Drawing.Point(3, 207);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(124, 90);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn6.Location = new System.Drawing.Point(263, 105);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(124, 96);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn5.Location = new System.Drawing.Point(133, 105);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(124, 96);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn4.Location = new System.Drawing.Point(3, 105);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(124, 96);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn3.Location = new System.Drawing.Point(263, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(124, 96);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn2.Location = new System.Drawing.Point(133, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(124, 96);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnReiniciar.Location = new System.Drawing.Point(194, 376);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(137, 41);
+            this.btnReiniciar.TabIndex = 1;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = false;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(552, 63);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(93, 20);
+            this.lblX.TabIndex = 2;
+            this.lblX.Text = "Victorias X :";
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.Location = new System.Drawing.Point(552, 131);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(94, 20);
+            this.lblO.TabIndex = 3;
+            this.lblO.Text = "Victorias O :";
+            // 
+            // lblEmpate
+            // 
+            this.lblEmpate.AutoSize = true;
+            this.lblEmpate.Location = new System.Drawing.Point(552, 191);
+            this.lblEmpate.Name = "lblEmpate";
+            this.lblEmpate.Size = new System.Drawing.Size(77, 20);
+            this.lblEmpate.TabIndex = 4;
+            this.lblEmpate.Text = "Empates:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEmpate);
+            this.Controls.Add(this.lblO);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblO;
+        private System.Windows.Forms.Label lblEmpate;
+    }
+}
+
---
+++ tps/tp3/grupo4/ejercicio11/T.PN°3_ejercicio_11/T.PN°3_ejercicio_11/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T.PN_3_ejercicio_11
+{
+    public partial class Form1 : Form
+    {
+        bool turnoX = true;
+        bool hayGanador = false;
+        int victoriasX = 0;
+        int victoriasO = 0;
+        int empates = 0;
+        int jugadas = 0;
+        public Form1()
+        {
+            InitializeComponent();
+         
+          
+        }
+        private bool HayGanador()
+        {
+            if (btn1.Text != "" && btn1.Text == btn2.Text && btn1.Text == btn3.Text)
+            {
+                return true;
+            }
+            if (btn4.Text != "" && btn4.Text == btn5.Text && btn4.Text == btn6.Text)
+            {
+                return true;
+            }
+            if (btn7.Text != "" && btn7.Text == btn8.Text && btn7.Text == btn9.Text)
+            {
+                return true;
+            }
+            if (btn1.Text != "" && btn1.Text == btn4.Text && btn1.Text == btn7.Text)
+            {
+                return true;
+            }
+            if (btn2.Text != "" && btn2.Text == btn5.Text && btn2.Text == btn8.Text)
+            {
+                return true;
+            }
+            if (btn3.Text != "" && btn3.Text == btn6.Text && btn3.Text == btn9.Text)
+            {
+                return true;
+            }
+            if (btn1.Text != "" && btn1.Text == btn5.Text && btn1.Text == btn9.Text)
+            {
+                return true;
+            }
+            if (btn3.Text != "" && btn3.Text == btn5.Text && btn3.Text == btn7.Text)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+        
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (hayGanador)
+            {
+                return;
+            }
+            Button btn = (Button)sender;
+            if (btn.Text == "")
+            {
+                if (turnoX)
+                {
+                    btn.Text = "X";
+                    turnoX = false;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    turnoX = true;
+                }
+                jugadas++;
+            }
+            if(HayGanador())
+            {
+                if (turnoX) 
+                {
+                    MessageBox.Show("Ganó el jugador O");
+                    victoriasO++;
+                }
+                else
+                {
+                    MessageBox.Show("Ganó el jugador X");
+                    victoriasX++;
+                }
+                hayGanador = true;
+            }else if(jugadas ==9)
+            {
+                MessageBox.Show("¡Perdieron juntos, es un empate!");
+                empates++;
+            }
+            lblX.Text = "Victorias X: " + victoriasX;
+            lblO.Text = "Victorias O: " + victoriasO;
+            lblEmpate.Text = "Empates: " + empates;
+
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+            hayGanador = false;
+            turnoX = true;
+            jugadas = 0;
+        }
+    }
+}
