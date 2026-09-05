71c7fea7263af23b3ced34c985513d6ee99c6324
BBerasain <146792109+BBerasain@users.noreply.github.com>
2026-09-05 19:15:04 -0300

    ej6

---
+++ tps/tp3/grupo4/ejercicio6/ejercicio6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Ejercicio6
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese la cantidad de números de Fibonacci: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int anterior = 0;
+        int actual = 1;
+
+        Console.WriteLine($"Los primeros {n} números de Fibonacci son:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(anterior + " ");
+
+            int siguiente = anterior + actual;
+            anterior = actual;
+            actual = siguiente;
+        }
+    }
+}
