4b545ed5f5fc0aafa7704de7be63039214552b46
barrios115 <42774124+barrios115@users.noreply.github.com>
2026-09-13 22:48:22 -0300

    Add files via upload

---
+++ tps/tp4/grupo4/ejercicio 1 recursividad/ejercicio 1 recursividad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void MostrarNumeros(int numero)
+    {
+        if (numero == 0)
+        {
+            return;
+        }
+
+        Console.Write(numero + " ");
+
+        MostrarNumeros(numero - 1);
+    }
+
+    static void Main()
+    {
+        MostrarNumeros(10);
+
+        Console.ReadKey();
+    }
+}
